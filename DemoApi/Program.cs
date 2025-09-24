12e7c008b106809d4679a98f4e19e7f927070190
João Pedro Barbosa <43485533+jpbarbosa44@users.noreply.github.com>
2025-09-24 15:43:20 -0300

    test 4 jenkins commit

--- DemoApi/Program.cs
+++ DemoApi/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
+//
 app.MapGet("/", () => "Hello CI/CD!");
 app.MapGet("/soma/{a:int}/{b:int}", (int a, int b) => a + b);
 
